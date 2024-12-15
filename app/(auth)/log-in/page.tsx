d7007f41c4c98e579cb6260b3abf0deee5c210c0
parkchanmi <mimi226kr@naver.com>
2024-12-16 00:48:38 +0900

    edit 4차

--- app/(auth)/log-in/page.tsx
+++ app/(auth)/log-in/page.tsx
@@ -20,12 +20,6 @@ export default function LogIn() {
       </div>
       <form action={dispatch} className="flex flex-col gap-3" >
         <div className="relative">
-          <div className="size-5 absolute mt-2 ml-2 text-neutral-600">
-          <svg data-slot="icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-            <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z"></path>
-            <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z"></path>
-          </svg>
-          </div>
           <Input
             name="email"
             type="email"
@@ -35,11 +29,6 @@ export default function LogIn() {
           />
         </div>
         <div className="relative">
-          <div className="size-5 absolute mt-2 ml-2 text-neutral-600">
-          <svg data-slot="icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-            <path clipRule="evenodd" fillRule="evenodd" d="M15.75 1.5a6.75 6.75 0 0 0-6.651 7.906c.067.39-.032.717-.221.906l-6.5 6.499a3 3 0 0 0-.878 2.121v2.818c0 .414.336.75.75.75H6a.75.75 0 0 0 .75-.75v-1.5h1.5A.75.75 0 0 0 9 19.5V18h1.5a.75.75 0 0 0 .53-.22l2.658-2.658c.19-.189.517-.288.906-.22A6.75 6.75 0 1 0 15.75 1.5Zm0 3a.75.75 0 0 0 0 1.5A2.25 2.25 0 0 1 18 8.25a.75.75 0 0 0 1.5 0 3.75 3.75 0 0 0-3.75-3.75Z"></path>
-          </svg>
-          </div>
           <Input
             name="password"
             type="password"
